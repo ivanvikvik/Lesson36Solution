8dbbe461ce20ba9e6d3675655097ab32701ecb7e
ivanvikvik <ivanvikvik@gmail.com>
2025-02-23 18:52:35 +0300

    Add project files.

---
+++ Task01Project/main.cpp
@@ -0,0 +1,25 @@
+#include "util.h"
+#include "algorithm.h"
+#define SIZE 5
+
+int main() {
+	int array[SIZE]{};
+
+	//cout << "Before array: " << convert(array, SIZE) << endl;
+	init(array, SIZE, -10, 10);
+	cout << "Array: " << convert(array, SIZE) << endl;
+	
+	int max = get_max(array, SIZE);
+	cout << "Max value of array: " << max << ".\n";
+
+	int min = get_min(array, SIZE);
+	cout << "Min value of array: " << min << ".\n";
+
+	double arithmetical_avg = calculate_arithmetical_avg(array, SIZE);
+	cout << "Arithmetical average is " << arithmetical_avg << ".\n";
+
+	double geometrical_avg = calculate_geometrical_avg(array, SIZE);
+	cout << "Geometrical average is " << geometrical_avg << ".\n";
+
+	return 0;
+}
---
+++ Task01Project/util.h
@@ -0,0 +1,6 @@
+#include <iostream>	// cout, cin, endl
+#include <string>	// to_string()
+using namespace std;
+
+void init(int* array, int length, int a, int b);
+string convert(int* array, int length);
